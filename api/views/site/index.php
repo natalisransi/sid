2192ae41a9769d36687fd5a39e4deb7094b9bb8f
Yusuf Ayuba <yusufayuba@live.com>
2020-06-08 12:55:12 +0800

    add api environment

---
+++ api/views/site/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'REST API SID';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Congratulations!</h1>
+
+        <p class="lead">REST API SID</p>
+
+    </div>
+
+    <div class="body-content">
+
+    </div>
+</div>
