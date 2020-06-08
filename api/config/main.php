13b93fd31c439843498ffac85a9f9e22488fcc77
Yusuf Ayuba <yusufayuba@live.com>
2020-06-08 13:09:44 +0800

    enable module for versioning rest api

--- api/config/main.php
+++ api/config/main.php
@@ -11,6 +11,12 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'api\controllers',
+    'modules' => [
+        'v1' => [
+            'basePath' => '@api/modules/v1',
+            'class' => 'api\modules\v1\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-api',
---
+++ api/modules/v1/Module.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace api\modules\v1;
+
+//use yii\base\Module AS yiiModule;
+
+/**
+ *
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'api\modules\v1\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+}
