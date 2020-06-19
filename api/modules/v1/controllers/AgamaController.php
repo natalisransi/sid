677524e160a2ef426dec87e352a1819c98396ebe
Yusuf Ayuba <yusufayuba@live.com>
2020-06-19 19:31:18 +0800

    add agama api

---
+++ api/modules/v1/controllers/AgamaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use api\modules\v1\components\BaseController;
+
+class AgamaController extends BaseController
+{
+    public $modelClass = '\common\models\Agama';
+}
