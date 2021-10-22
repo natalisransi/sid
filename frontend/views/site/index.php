fc7e62b41bec98c7f09d38648925a92313eed439
therinji <rindho2704@gmail.com>
2021-10-22 15:20:34 +0700

    #1

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -21,9 +21,9 @@ $this->title = 'My Yii Application';
                 <h2>Heading</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    dolore magna aliqua. Ut enimok ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariaturok.</p>
+                    fugiat nulla pariatur.</p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
